24ac6416b460bb8fe5f2a4fd81193c754b83773d
arontseggai <aron.tseggai@gmail.com>
2016-12-07 15:42:59 +0100

    Completed homepage

--- js/homemade.js
+++ js/homemade.js
@@ -10,3 +10,28 @@ $(window).on('scroll', function(){
     nav.removeClass('scrolled');
   }
 });
+
+
+$('#contact').on('click',function(){
+  var innerHeight           = $('.inner').outerHeight(),
+      outerContainer        = $('.outer'),
+      outerContainerHeight  = $('.outer').height(),
+      header                = $('#nav');
+
+  var removeOpenClass =function(){
+    header.removeClass('open');
+  }
+
+  if(outerContainerHeight < 1 ){
+    outerContainer.height(innerHeight * 0.5);
+    outerContainer.height(0);
+    outerContainer.height(innerHeight * 0.8);
+    outerContainer.height(innerHeight);
+
+    header.addClass('open');
+  } else {
+    outerContainer.height(0);
+    setTimeout(function(){removeOpenClass();}, 200)
+  }
+});
+
